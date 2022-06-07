218264ae0239a9c5016edaa45769f152bb913734
luckydonald <mac2019.git@luckydonald.de>
2022-06-07 22:31:30 +0200

    Fixed bugs with the help of monkey testing.

--- hello-hg.py
+++ hello-hg.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-
 __author__ = 'luckydonald'
-
-import logging
-
-logger = logging.getLogger(__name__)
 
 import re
 from mercurial import error
+from mercurial.ui import ui as MercurialUI
 
 
 ISSUE_REGEX = re.compile(r'#\d+')
+ERROR_MESSAGE = b'Commit description is Missing Hacknplan task ID (e.g. #123)'
 
 
 def check_commit_message_has_issue_number_hook(ui, repo, **kwargs):
@@ -22,49 +19,58 @@ def check_commit_message_has_issue_number_hook(ui, repo, **kwargs):
     project you want to check, or to your (global( user hgrc to apply to all projects.
 
     [hooks]
-    precommit = python:path/to/script/hello-hg.py:check_commit_message_has_issue_number_hook
+    pretxncommit.hello-hg = python:path/to/script/hello-hg.py:check_commit_message_has_issue_number_hook
+
+    :type ui: MercurialUI
     """
     hg_commit_message = repo['tip'].description()
 
     if not check_commit_message_has_issue_number(hg_commit_message):
-        raise error.Abort("Commit description is Missing Hacknpan task ID (e.g. #123)")
-    else:
-        return True
+        continue_anyway = ui.promptchoice(ERROR_MESSAGE + b"\nDo you want to commit anyway?" + repr(hg_commit_message) + "  $$ &Yes $$ &No", default=1) == 0
+        if not continue_anyway:
+            raise error.Abort(ERROR_MESSAGE + ": User aborted commit.")
+        # end if
+    # end if
+    return 0  # linux error codes, so 0/False/None = ok, True/1 = error
+# end def
 
 
 def check_commit_message_has_issue_number(commit_message_text):
     """
     Check if the message contains a issue reference somewhere.
 
-    >>> check_commit_message_has_issue_number('foo bar #1234 bar foo')
+    >>> check_commit_message_has_issue_number(b'foo bar #1234 bar foo')
     True
 
-    >>> check_commit_message_has_issue_number('#1234 bar foo')
+    >>> check_commit_message_has_issue_number(b'#1234 bar foo')
     True
 
-    >>> check_commit_message_has_issue_number('#1234 bar foo')
+    >>> check_commit_message_has_issue_number(b'#1234 bar foo')
     True
 
-    >>> check_commit_message_has_issue_number("First line\\nSecond line\\nLast line with #1234 reference.")
+    >>> check_commit_message_has_issue_number(b"First line\\nSecond line\\nLast line with #1234 reference.")
     True
 
-    >>> check_commit_message_has_issue_number("#1234.")
+    >>> check_commit_message_has_issue_number(b"#1234.")
     True
 
-    >>> check_commit_message_has_issue_number("#1234a")
+    >>> check_commit_message_has_issue_number(b"prefix#1234")
+    True
+
+    >>> check_commit_message_has_issue_number(b"#1234suffix")
+    True
+
+    >>> check_commit_message_has_issue_number(b"#.1234")
     False
 
-    >>> check_commit_message_has_issue_number("#.1234")
+    >>> check_commit_message_has_issue_number(b"# 1234")
     False
 
-    >>> check_commit_message_has_issue_number("# 1234")
-    False
-
-    >>> check_commit_message_has_issue_number("Issue #a")
+    >>> check_commit_message_has_issue_number(b"Issue #a")
     False
 
     :param commit_message_text: The commit message to check
-    :type  commit_message_text: str
+    :type  commit_message_text: bytes
     :return:
     """
     match = ISSUE_REGEX.search(commit_message_text)
